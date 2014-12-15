4eb240e81a91798242cc947735b3ce3e76dc3c7c
The Red Majora <theredmajora@gmail.com>
2014-12-15 19:33:37 +0100

    Added spawns for the Land Octoroks
    They spawn in plains and swampland.
    
    Also removed the battlegear_at.cfg

--- src/main/java/addon/zeldaswordskills/entity/AddonEntities.java
+++ src/main/java/addon/zeldaswordskills/entity/AddonEntities.java
@@ -1,8 +1,10 @@
 package addon.zeldaswordskills.entity;
 
+import net.minecraft.entity.EnumCreatureType;
+import net.minecraft.world.biome.BiomeGenBase;
+import zeldaswordskills.entity.CustomEntityList;
 import addon.zeldaswordskills.ZSSAddon;
 import cpw.mods.fml.common.registry.EntityRegistry;
-import zeldaswordskills.entity.CustomEntityList;
 
 public class AddonEntities
 {
@@ -10,6 +12,7 @@ public class AddonEntities
 	{
 		registerEntities();
 		addEntityMaps();
+		registerEntitySpawns();
 	}
 	
 	public static void registerEntities()
@@ -21,5 +24,11 @@ public class AddonEntities
 	public static void addEntityMaps()
 	{
 		CustomEntityList.addMapping(EntityLandOctorok.class, "landOctorok", 0xFFFFFF, 0xFF0000, 0xFFFFFF, 0x0066FF);
+	}
+	
+	public static void registerEntitySpawns()
+	{
+		EntityRegistry.addSpawn(EntityLandOctorok.class, 4, 1, 5, EnumCreatureType.ambient, BiomeGenBase.plains);
+	    EntityRegistry.addSpawn(EntityLandOctorok.class, 8, 1, 5, EnumCreatureType.ambient, BiomeGenBase.swampland);
 	}
 }
